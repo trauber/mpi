9639f034f906fdddb414120ca456736fac34cef8
Richard Traube <rich.traube@gmail.com>
2012-07-19 17:34:20 -0400

    Added paper checkboxes.

--- README.md
+++ README.md
@@ -7,6 +7,26 @@ Current test data includes only a subset of the indexed years for the
 News Chronicle.  Eventually we'll have more data for that paper and also
 data for the Moorestown News, the Newsweekly, and the Moorestown Sun.
 
+## Data Cleanup
+
+See a video explaining data cleanup at
+<http://dl.dropbox.com/u/21499508/google-refine.m4v> .
+
+Primary issues:
+
+- many typos in dates
+- out of type info in the two sort fields, dates and page numbers
+- newlines embedded in some fields; possible causes:
+	- word wrap on in office and multiple spaces result in newlines
+	- cut and paste into fields?
+
+I don't have ms-office.  It's possible that excel treats embedded newlines
+correctly.  However, libreoffice calc and gnumeric do not.  I found a
+c program for producing .tsv that treats the embedded lines correctly at
+<http://wizard.ae.krakow.pl/~jb/xls2txt/> .
+
+
+
 ## TODO
 
 - check for term length
@@ -14,7 +34,9 @@ data for the Moorestown News, the Newsweekly, and the Moorestown Sun.
 - add checkboxes for newspapers with News Chronicle and Moorestown Sun checked by default
 - add mustache for list output, no columns and search page widget for it on entry page
 - validate date format (iso 8601 YYYY-MM-DD)
-- add spage column to table
+- <span style="text-decoration: line-through">add spage column to table</span>
+- clen data with google refine
+
 
 
 
--- search.php
+++ search.php
@@ -8,20 +8,66 @@ $phototext = '<img src="photo.png" height="14" align="bottom"/>';
 # Using a simple templating library, mustache, which is implemented in about a dozen different languages.
 # php mustache is from bobthecow....
 include('Mustache.php');
+
+
+# rendering function.
+function mrender($t,$v) {
+	$m = new Mustache;
+	echo $m->render($t, $v);
+	exit;
+}
+
+
+# Found a nice date checker snippet at php.net!!
+function datecheck($input) {
+	$date_format = 'Y-m-d';
+	$input = trim($input);
+	$time = strtotime($input);
+	$is_valid = date($date_format, $time) == $input;
+
+	if (!($is_valid)) {
+		$template = file_get_contents('exception.mustache');
+		$view['exception'] = "$input is not valid.  Dates must be in the form: YYYY-MM-DD. Example: 1973-04-08.  Dates must also be real.";
+		mrender($template, $view);
+	}
+}
+
+
+
+# This is the results template.
+# For exceptions use a different one.
+# And make it something simple, with a return link to the search form
+# and a one scalar view.
 $template = file_get_contents('search.mustache');
 
-$con = mysql_connect ("<server>", "<login>","<password>")  or die (mysql_error());
+$con = mysql_connect ("127.0.0.1", "root","7he2@7@sp12")  or die (mysql_error());
 
 $datesort = $_POST['dsort'];
 $term = $_POST['term'];  
 $startdate = $_POST['startdate'];
 $enddate = $_POST['enddate'];
+$paper = $_POST['paper'];
+
+datecheck($startdate);
+datecheck($enddate);
+
+
+if (strlen($term) > 100) {
+	$template = file_get_contents('exception.mustache');
+	$view['exception'] = "You must shorten your search term.  It is over 100 characters.";
+	mrender($template, $view);
+}
+
+
+
 
 $view = array();
 $view['datesort'] = $datesort;
 $view['term'] = $term;
 $view['startdate'] = $startdate;
 $view['enddate'] = $enddate;
+
+
 
 $results = array();
 
@@ -44,10 +90,9 @@ $sql = mysql_query("
 $count = 0;
 
 while ($row = mysql_fetch_array($sql)){
-	$count++;
+
 	if(isset($_POST['photo']) && $_POST['photo'] == 'Yes') {
 		if ($row['photo'] == '') {
-			$count--;
 			continue;
 		}
 	}
@@ -56,21 +101,37 @@ while ($row = mysql_fetch_array($sql)){
 		$row['photo'] = $phototext;
 	}
 
-	array_push($results, array(
-		'count' => $count,
-		'headline' => $row['headline'],
-		'newspaper' => $row['newspaper'],
-		'date' => $row['date'],
-		'page' => $row['page'],
-		'photo' => $row['photo'],
-		'subject' => $row['subject'],
-		'columntitle' => $row['columntitle']
-	));
+	foreach($paper as $k=>$v) {
+		if(isset($paper[$k])) {
+			$count++;
+			if ($row['newspaper'] != $v) {
+				$count--;
+				continue;
+			} else {
+
+			array_push($results, array(
+			'headline' => $row['headline'],
+			'newspaper' => $row['newspaper'],
+			'date' => $row['date'],
+			'page' => $row['page'],
+			'photo' => $row['photo'],
+			'subject' => $row['subject'],
+			'columntitle' => $row['columntitle'],
+			'count' => $count
+			));
+			}
+		}
+
+	}
+
+
+
 	if ($count > $max) {
 		unset($results);
-		$view['maxout'] = true;
-		$count = "Over " . $max;
-		break;
+		$template = file_get_contents('exception.mustache');
+		$view['exception'] = "Your search returned more than $max results.  Please refine it.";
+		mrender($template, $view);
+
 	}
 }
 
@@ -78,12 +139,6 @@ while ($row = mysql_fetch_array($sql)){
 $view['count'] = $count;
 $view['results'] = $results;
 
-
-
-$m = new Mustache;
-
-echo $m->render($template, $view);
-
-
+mrender($template, $view);
 
 ?>
